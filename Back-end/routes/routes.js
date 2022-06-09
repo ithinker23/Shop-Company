48e4a3fc39be878498da2a3597fc75ba23f90c49
ithinker23 <44293948+ithinker23@users.noreply.github.com>
2022-06-09 00:21:11 -0700

    changed backend

--- Back-end/routes/routes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const path = require('path');
-const controller = require('../controllers/controller.js');
-
-module.exports = (app) => {
-    app.route('/checkLoginHandler').get(controller.checkLogin);
-}
--- Back-end/server.js
+++ Back-end/server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-const routes = require('./api_routes/routes');
-routes(app);
+const loginCheckerRoutes = require('./routes/loginChecker');
+loginCheckerRoutes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
