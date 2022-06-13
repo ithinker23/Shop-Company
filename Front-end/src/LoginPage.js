ade8c7496e1a87f210c99a77e8378392b585f5c1
ithinker23 <44293948+ithinker23@users.noreply.github.com>
2022-06-12 18:53:15 -0700

    ui

--- Front-end/src/LoginPage.js
+++ Front-end/src/LoginPage.js
@@ -18,22 +18,33 @@ export default function LoginPage() {
       try {
         var response = await Axios.post(backendURL + '/validify/login', formData)
 
-        notifsRef.current.showNotifs(response.data.title,response.data.msg)
+        notifsRef.current.showNotifs(response.data.title, response.data.msg)
 
         window.location = "/"
       } catch (err) {
-        notifsRef.current.showNotifs(err.response.data.title,err.response.data.msg)
+        notifsRef.current.showNotifs(err.response.data.title, err.response.data.msg)
       }
 
     } else {
-      notifsRef.current.showNotifs("Login Failed","Input field(s) are empty")
+      notifsRef.current.showNotifs("Login Failed", "Input field(s) are empty")
     }
   }
   return (
     <>
-      <input ref={usernameRef} type="text" ></input>
-      <input ref={passwordRef} type="password" ></input>
-      <button type='submit' onClick={checkLoginHandler}>Click Me</button>
+      <div className='textField'>
+        <input ref={usernameRef} type="text" />
+        <span className="bar"></span>
+        <label> Username </label>
+      </div>
+      <div className='textField'>
+        <input ref={passwordRef} type="password" />
+        <span className="bar"></span>
+        <label> Password </label>
+      </div>
+      <div className='button' onClick={checkLoginHandler}>
+        <div className="slide"></div>
+        <div className='buttonText'>Login</div>
+      </div>
       <Notifs ref={notifsRef} />
     </>
   )
--- Front-end/src/RegisterPage.js
+++ Front-end/src/RegisterPage.js
@@ -3,40 +3,56 @@ import Axios from 'axios';
 import Notifs from './Notifs'
 
 export default function RegisterPage() {
-  
-  var newUserNameRef=useRef()
-  var newEmailRef=useRef()
-  var newPasswordRef=useRef()
+
+  var newUserNameRef = useRef()
+  var newEmailRef = useRef()
+  var newPasswordRef = useRef()
 
   const notifsRef = useRef();
 
   const backendURL = 'http://localhost:5000';
 
-  async function checkRegisterHandler(){
+  async function checkRegisterHandler() {
     if (!(newUserNameRef.current.value === "" || newEmailRef.current.value === "" || newPasswordRef.current.value === "")) {
 
-      const formData = { Username: newUserNameRef.current.value, Email: newEmailRef.current.value, Password: newPasswordRef.current.value}
-      try{
+      const formData = { Username: newUserNameRef.current.value, Email: newEmailRef.current.value, Password: newPasswordRef.current.value }
+      try {
         var response = await Axios.post(backendURL + '/validify/register', formData)
         notifsRef.current.showNotifs(response.data.title, response.data.msg)
         window.location = "/login"
-      }catch (err){
+      } catch (err) {
         notifsRef.current.showNotifs(err.response.data.title, err.response.data.msg)
 
       }
 
-    }else{
+    } else {
       notifsRef.current.showNotifs("Registration Failed", "Input field(s) are empty")
     }
   }
 
   return (
     <>
-      <input ref={newUserNameRef} type="text" ></input>
-      <input ref={newEmailRef} type="email" ></input>
-      <input ref={newPasswordRef} type="password" ></input>
-      <button type='submit' onClick={checkRegisterHandler}>Click Me</button>
-      <Notifs ref={notifsRef}/>
+      <div className='textField'>
+        <input ref={newUserNameRef} type="text" />
+        <span className="bar"></span>
+        <label> Username </label>
+      </div>
+      <div className='textField'>
+        <input ref={newEmailRef} type="email" />
+        <span className="bar"></span>
+        <label> Email </label>
+      </div>
+      <div className='textField'>
+        <input ref={newPasswordRef} type="password" />
+        <span className="bar"></span>
+        <label> Password </label>
+      </div>
+
+      <div className='button' onClick={checkRegisterHandler}>
+        <div className="slide"></div>
+        <div className='buttonText'>Register</div>
+      </div>
+      <Notifs ref={notifsRef} />
     </>
   )
 }
