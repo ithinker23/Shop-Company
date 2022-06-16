fca19c32ff4fc9a64442e5b78cbd7dfbe36ca8dd
Ryan <76890786+ryanyae@users.noreply.github.com>
2022-06-16 04:31:48 -0700

    fixed filter

--- Back-end/model/itemsQuerys.js
+++ Back-end/model/itemsQuerys.js
@@ -15,22 +15,28 @@ function getItems(filters) {
 
         if (filters.Classes.length > 0 || filters.Colours.length > 0) query += " WHERE ";
 
-        for (var i = 0; i < filters.Classes.length; i++) {
-
-            if (i != 0) query += "OR ";
-            query += "Class = " + "'" + filters.Classes[i] + "' ";
-
+        if (filters.Classes.length == 0) {
             for (var x = 0; x < filters.Colours.length; x++) {
-
-                if (x != 0) {
-                    query += "OR ";
-                } else if (filters.Classes.length > 0) query += " AND ";
-
+                if (x > 0) query += " OR ";
                 query += "Colours = " + "'" + filters.Colours[x] + "' ";
+            
+            }
+        } else {
+            for (var i = 0; i < filters.Classes.length; i++) {
+
+                if (i != 0) query += "OR ";
+                query += "Class = " + "'" + filters.Classes[i] + "' ";
+
+                for (var x = 0; x < filters.Colours.length; x++) {
+
+                    if (x != 0) {
+                        query += "OR ";
+                    } else if (filters.Classes.length > 0) query += " AND ";
+
+                    query += "Colours = " + "'" + filters.Colours[x] + "' ";
+                }
             }
         }
-
-
 
         console.log("SELECT * FROM items" + query)
         DBConnection.query("SELECT * FROM items" + query, (err, res) => {
